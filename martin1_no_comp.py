fa37f74506d013f62d40ee9f0308efa01ffc7aa1
N2WU <49031851+N2WU@users.noreply.github.com>
2024-04-23 15:04:50 -0400

    Update martin1_no_comp.py

--- martin1_no_comp.py
+++ martin1_no_comp.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
-import sounddevice as sd
+#import sounddevice as sd
 import matplotlib.pyplot as plt
+import scipy.fftpack as scifft
 
 def martin1(img_array,fs):
     ms_pix = 0.4576/1000
@@ -94,10 +95,53 @@ def martin1_decode(r,fs):
 
     return r_img_array
 
+def dct2(a):
+    return scifft.dct(scifft.dct(a.T, norm='ortho').T, norm='ortho')
+
+def idct2(a):
+    return scifft.idct(scifft.idct(a.T, norm='ortho').T, norm='ortho')    
+
+def dct_fun(img_array_raw, nlines=256, npix=320):
+    dct_array = np.zeros_like(img_array_raw)#np.zeros((nlines,npix,3))
+    # convert to YCbCr
+
+    # Block Splitting
+
+    # DCT
+    # maybe try this without the pixel
+    #for color in range(3):
+    dct_array = dct2(img_array_raw)
+    
+    # Quantization
+
+    # Huffman Coding
+
+    return dct_array
+
+def idct_fun(dct_array, nlines=256, npix=320):
+    # inverse DCT
+    img_array = np.zeros_like(dct_array) #np.zeros((nlines,npix,3))
+    # maybe try this without the pixel
+    #for color in range(3):
+    img_array= idct2(dct_array)   
+
+    # decompress
+
+    # convert back to RGB?
+    return img_array
+
+def calc_mse(input,output):
+    input = np.reshape(input,(-1,1))/np.amax(input)
+    output = np.reshape(output,(-1,1))/np.amax(input)
+    mse = 10 * np.log10(
+            np.mean(np.abs(input - output) ** 2)
+        )
+    return mse
+
 if __name__ == "__main__":
     # constants
     fs = 48e3
-    sd.default.samplerate = fs
+    #sd.default.samplerate = fs
     
     # load image, cropped no compression
     img = Image.open('martin1_crop.jpg')
@@ -106,15 +150,45 @@ if __name__ == "__main__":
     #sd.play(martin1_sig,fs)
     #sd.wait()
     # simulate at 5dB in AWGN channel
+    snr_db = 10
+    noise = np.random.randn(len(martin1_sig)) / 10**(snr_db/10)
+    martin1_sig = martin1_sig + noise
+    #r_img_array = martin1_decode(martin1_sig,fs)
+    # r_img_array = r_img_array.astype(np.uint8)
+    #r_img = Image.fromarray(r_img_array)
+    #r_img.save("data/martin_rx_nocomp.jpg")
+    #r_img.show()
+
+    #mse = calc_mse(img_array, r_img_array)
+    #print("SNR: ", snr_db, " dB. No Compression MSE: ", mse, " dB")
+
+    # load image, raw
+    # goal here is to take same transmission time (same image size) but yield higher quality
+    # different ways to do this:
+    # 1. resize big image into 256x320 (sort of edging out whatever compression tool
+    # 2. transmit DCT components instead of RGB values, then IDCT
+    # 3. 
+    img = Image.open('martin1_raw.jpg')
+    img_array_raw = np.asarray(img)
+    # apply DCT to format to 256x320x3 image
+    nlines = 256
+    npix = 320
+    img_array_raw = img_array_raw[:,:,0]
+    img_array_raw.flatten()
+    dct_array = dct_fun(img_array_raw)
+    img_array = idct_fun(dct_array)
+    img_array = img_array.astype(np.uint8)
+    img = Image.fromarray(img_array)
+    #img.show()
+    martin1_sig = martin1(img_array,fs)
     snr_db = 5
     noise = np.random.randn(len(martin1_sig)) / 10**(snr_db/10)
     martin1_sig = martin1_sig + noise
     r_img_array = martin1_decode(martin1_sig,fs)
+    r_img_array = r_img_array.astype(np.uint8)
     r_img = Image.fromarray(r_img_array)
-    r_img.save("data/martin_rx_nocomp.jpg")
     r_img.show()
+    mse = calc_mse(img_array, r_img_array)
+    print("SNR: ", snr_db, " dB. Compression MSE: ", mse, " dB")
 
-    # load image, raw
-    #img = Image.open('martin1_crop.jpg')
-    #img_array = np.asarray(img)
-    #martin1_sig = martin1(img_array,fs)
+
